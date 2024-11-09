8eb1ad6dd340598cf64addfa40ab56cc99e444d1
dabbiru <shivakumardabbiru@gmail.com>
2024-11-09 21:01:59 +0100

    Add additional logic to format the data in the extent reports.

--- BlogPostValidation/src/test/java/Reporting/ExtentReportManager.java
+++ BlogPostValidation/src/test/java/Reporting/ExtentReportManager.java
@@ -9,6 +9,8 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import Utilities.ReusableFunctions;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import io.restassured.specification.QueryableRequestSpecification;
 
@@ -77,7 +79,7 @@ public class ExtentReportManager implements ITestListener {
             test.info("Request Details:");
             test.info("Endpoint: " + qrb.getBaseUri());
             test.info("Request Method: " + qrb.getMethod());
-            test.info("Request Headers: " + qrb.getHeaders());
+            logReqeustHeaders(qrb);
             test.info("Request Body: " + (qrb.getBody() != null ? qrb.getBody().toString() : "No body"));
         }
     }
@@ -86,8 +88,31 @@ public class ExtentReportManager implements ITestListener {
     public static void logResponse(Response response, String endpoint) {
         if (test != null) {
             test.info("Status Code: " + response.getStatusCode());
-            test.info("Response Headers: " + response.getHeaders().asList().toString());
-            test.info("Response Body (Formatted JSON): " + response.asPrettyString());
+            String prettyJson = response.getBody().prettyPrint();
+            test.info("Response Body (Formatted JSON):\n" + "<pre>" + prettyJson + "</pre>");
         }
+    }
+    
+    
+    public static void logReqeustHeaders(QueryableRequestSpecification qrb) {
+        Headers headers = qrb.getHeaders();
+
+        // Start building the HTML table
+        StringBuilder tableBuilder = new StringBuilder();
+        tableBuilder.append("<table border='1'>")
+                    .append("<tr><th>Header Name</th><th>Header Value</th></tr>"); // Table headers
+
+        // Add rows for each header
+        for (Header header : headers) {
+            tableBuilder.append("<tr>")
+                        .append("<td>").append(header.getName()).append("</td>")
+                        .append("<td>").append(header.getValue()).append("</td>")
+                        .append("</tr>");
+        }
+
+        tableBuilder.append("</table>"); // Close table
+
+        // Log the table to the Extent Report
+        test.info("Request Headers:\n" + tableBuilder.toString());
     }
 }
